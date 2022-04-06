4e7e517e1832ef52219a8328d37849a7e0d68c71
mohammedass <mohammed.ass@campus.technion.ac.il>
2022-04-07 00:41:41 +0300

    fixed code

--- print_number.c
+++ print_number.c
@@ -2,9 +2,10 @@
 
 int course_number[6] = {2, 3, 4, 1, 2,4};
 int main() {
-    for(int i =  0; i < 6; i++) {
-        printf("%d", course_number[i]);
+    for(int i =  -1; i < 5; i++) {
+        printf("%d", course_number[i+1]);
     }
     printf("\n");
     return 0;
 }
+
