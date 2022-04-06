ecd9c75c156a27db2044408a1cf8348233c5828a
mohammedass <mohammed.ass@campus.technion.ac.il>
2022-04-07 01:04:01 +0300

    ficed code

--- print_number.c
+++ print_number.c
@@ -9,14 +9,3 @@ int main() {
     printf("\n");
     return 0;
 }
-
-=======
-int course_number[6] = {4, 2, 1, 4, 3, 2};
-int main() {
-	for(int i = 6; i > 0; i--) {
-		printf("%d", course_number[i-1]);
-	}
-	printf("\n");
-	return 0;
-}
->>>>>>> 3580e355c070c99c43a40a853e0373bdf4a2d810
