687b08ba68f2c9c783390a5c8e10dc4d8bb93392
mohammedass <mohammed.ass@campus.technion.ac.il>
2023-04-09 17:47:42 +0300

    correct printing

--- print_number.c
+++ print_number.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
 int course_number[6] = {2, 3, 4, 1, 2, 4};
-int main() {
-<<<<<<< HEAD
+int main(){
     for(int i =  0; i < 6; i++){
-=======
-    for(int i =  0; i < 6; i++){
->>>>>>> main
         printf("%d", course_number[i]);
     }
     printf("\n");
